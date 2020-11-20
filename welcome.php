9c026bb538586270130fddec8a2ae3ac7afee29e
WellWedley <WellWedley@users.noreply.github.com>
2020-11-20 14:56:59 +0100

    test

--- welcome.php
+++ welcome.php
@@ -6,24 +6,24 @@ include 'session.php';
 <head>
 	<meta content='text/html; charset=UTF-8' http-equiv='Content-Type'/>
 	<link rel="stylesheet" type="text/css" href="style.css" />
-	<title>7topics - Login Demo</title>
+	<title>AEPC</title>
 </head>
 <body>
 <header>
 	<nav>
 		<ul>
-			<li><a href="https://7topics.com">7topics</a></li>
-			<li><a href="https://7topics.com/creating-user-profile-page-using-php-and-mysql.html">Tutorial</a></li>
+			<li><a href="index.php">Accueil</a></li>
+			<li><a href="">Lien 2</a></li>
 		</ul>
 	</nav>
 </header>
 <div id="center">
-<div id="center-set"> -written by Rahul Ranjan
-<h1 align='center'>Welcome <?php echo $loggedin_session; ?>,</h1>
-You are now logged in. you can logout by clicking on signout link given below.
+<div id="center-set">
+<h1 align='center'>Bienvenue  <?php echo $loggedin_session; ?>,</h1>
+Vous êtes maintenant en ligne.
 <div id="contentbox">
 <?php
-$sql = "SELECT * FROM member where mem_id=$loggedin_id";
+$sql = "SELECT * FROM animateurs where mail_anim=$loggedin_id";
 $result = mysqli_query($con, $sql);
 ?>
 <?php
@@ -32,33 +32,33 @@ while ($rows = mysqli_fetch_array($result)) {
 <div id="signup">
 <div id="signup-st">
 <form action="" method="POST" id="signin" id="reg">
-<div id="reg-head" class="headrg">Your Profile</div>
+<div id="reg-head" class="headrg">Votre profil</div>
 <table border="0" align="center" cellpadding="2" cellspacing="0">
 <tr id="lg-1">
-<td class="tl-1"> <div align="left" id="tb-name">Reg id:</div> </td>
-<td class="tl-4"><?php echo $rows['mem_id']; ?></td>
+<td class="tl-1"> <div align="left" id="tb-name">Identifiant:</div> </td>
+<td class="tl-4"><?php echo $rows['id_anim']; ?></td>
 </tr>
 <tr id="lg-1">
-<td class="tl-1"><div align="left" id="tb-name">Username:</div></td>
-<td class="tl-4"><?php echo $rows['username']; ?></td>
+<td class="tl-1"><div align="left" id="tb-name">Pseudo :</div></td>
+<td class="tl-4"><?php echo $rows['pseudo']; ?></td>
 </tr>
 <tr id="lg-1">
 <td class="tl-1"><div align="left" id="tb-name">Name:</div></td>
-<td class="tl-4"><?php echo $rows['fname']; ?> <?php echo $rows['lname']; ?></td>
+<td class="tl-4"><?php echo $rows['prenom']; ?> <?php echo $rows['nom']; ?></td>
 </tr>
 <tr id="lg-1">
-<td class="tl-1"><div align="left" id="tb-name">Email id:</div></td>
-<td class="tl-4"><?php echo $rows['address']; ?></td>
+<td class="tl-1"><div align="left" id="tb-name">Email :</div></td>
+<td class="tl-4"><?php echo $rows['email']; ?></td>
 </tr>
 </table>
-<div id="reg-bottom" class="btmrg">Copyright &copy; 2015 7topics.com</div>
+<div id="reg-bottom" class="btmrg"></div>
 </form>
 </div>
 </div>
 <div id="login">
 <div id="login-sg">
-<div id="st"><a href="logout.php" id="st-btn">Sign Out</a></div>
-<div id="st"><a href="deleteac.php" id="st-btn">Delete Account</a></div>
+<div id="st"><a href="logout.php" id="st-btn">Se déconnecter</a></div>
+<div id="st"><a href="deleteac.php" id="st-btn">Supprimer le compte</a></div>
 </div>
 </div>
 <?php
@@ -69,6 +69,6 @@ while ($rows = mysqli_fetch_array($result)) {
 </div>
 </div>
 </br>
-<div id="footer"><p> Copyright &copy; 2014-2015 7topics.com </p></div>
+<div id="footer"><p> Amitié Cévenole</p></div>
 </body>
 </html>
