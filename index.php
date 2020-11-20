23d4462065f30e87c37f14a372c3ae5e38b4c174
WellWedley <WellWedley@users.noreply.github.com>
2020-11-20 11:44:02 +0100

    Mise en page

--- index.php
+++ index.php
@@ -15,13 +15,14 @@
 		</ul>
 	</nav>
 </header>
+<h1 class='titleh1'> Association d'Education Populaire Cévenole</h1>
 
-<h1 class='title1'> Association d'Education Populaire Cévenole</h1>
 <div id="center">
 <div id="center-set">
 <div id="signup">
 <div id="signup-st">
-<div align="center">
+<div style="align:center">
+
 <?php
 $remarks = isset($_GET['remarks']) ? $_GET['remarks'] : '';
 if ($remarks == null and $remarks == "") {
@@ -42,32 +43,32 @@ if ($remarks == 'error') {
 <table border="0" align="center" cellpadding="2" cellspacing="0">
 <tr>
 <td class="t-1">
-<div align="left" id="tb-name">Prénom:</div>
+<div align="left" id="tb-name">Prénom :</div>
 </td>
 <td width="171">
 <input type="text" name="fname" id="tb-box"/>
 </td>
 </tr>
 <tr>
-<td class="t-1"><div align="left" id="tb-name">Nom :</div></td>
+<td class="t-1"><div align="left" id="tb-name">Nom : </div></td>
 <td><input type="text" name="lname" id="tb-box"/></td>
 </tr>
 <tr>
-<td class="t-1"><div align="left" id="tb-name">Email :</div></td>
+<td class="t-1"><div align="left" id="tb-name">Email : </div></td>
 <td><input type="text" id="tb-box" name="address" /></td>
 </tr>
 <tr>
-<td class="t-1"><div align="left" id="tb-name">Prénom:</div></td>
+<td class="t-1"><div align="left" id="tb-name">Pseudo : </div></td>
 <td><input type="text" id="tb-box" name="username" /></td>
 </tr>
 <tr>
-<td class="t-1"><div align="left" id="tb-name">M. de passe :</div></td>
+<td class="t-1"><div align="left" id="tb-name">Mot de passe : </div></td>
 <td><input id="tb-box" type="password" name="password" /></td>
 </tr>
 </table>
-<div id="st"><input name="submit" type="submit" value="Submit" id="st-btn"/></div>
+<div id="st"><input name="submit" type="submit" value="Envoyer" id="st-btn"/></div>
 </form>
-<div id="reg-bottom" class="btmrg">Copyright &copy; 2015 7topics.com</div>
+
 </div>
 </div>
 <div id="login">
@@ -84,15 +85,15 @@ if ($remarks == 'failed') {
 ?>
 <table border="0" align="center" cellpadding="2" cellspacing="0">
 <tr id="lg-1">
-<td class="tl-1"><div align="left" id="tb-name">Username:</div></td>
+<td class="tl-1"><div align="left" id="tb-name">Username :</div></td>
 <td><input type="text" id="tb-box" name="username" /></td>
 </tr>
 <tr id="lg-1">
-<td class="tl-1"><div align="left" id="tb-name">Mot de passe:</div></td>
+<td class="tl-1"><div align="left" id="tb-name">Mot de passe :</div></td>
 <td><input id="tb-box" type="password" name="password" /></td>
 </tr>
 </table>
-<div id="st"><input name="submit" type="submit" value="Login" id="st-btn"/></div>
+<div id="st"><input name="submit" type="submit" value="Connexion" id="st-btn"/></div>
 </form>
 
 </div>
